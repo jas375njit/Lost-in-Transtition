39f33bc1ea908e8934e7af0fc068ddb0aaa685c6
Joel <jas375@njit.edu>
2026-04-21 19:14:16 -0400

    First Game Room commit (Only two players allowed)

--- GameRoom.java
+++ GameRoom.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class GameRoom{
   private static int [] wordLimit = {10,7,5};
-  private static String [] wordList = {"Apple", "Eagle", "Pen", "School"};
+  private static String [] wordList = {"Apple", "Eagle", "Flashlight", "Computer", "Paper"};
   private ArrayList<Socket> players = new ArrayList<>();
 
 public GameRoom (ArrayList<Socket> players){
@@ -20,26 +20,52 @@ public void addPlayer(Socket player){
 
   public void game(){
     try{
-      BufferedReader [] readers = new BufferedReader[players.size()];
-      PrintWriter [] writers = new PrintWriter [players.size()];
-      System.out.println("Lost in Transition: Game is about to start");
+      int amountOfPlayers = players.size();
 
-      String [] names = new String [players.size()];
+      //Player 1 reader and writer
+      BufferedReader readerPlayer1 = new BufferedReader(new InputStreamReader(players.get(0).getInputStream()));
 
-      for (int i = 0; i < players.size(); i++){
-        writers[i].println("Enter your name");
-        names[i] = readers[i].readLine();
-      }
+      PrintWriter writerPlayer1 = new PrintWriter(players.get(0).getOutputStream(),true);
+
+      //Player 2 reader and writer
+      BufferedReader readerPlayer2 = new BufferedReader(new InputStreamReader(players.get(1).getInputStream()));
+
+      PrintWriter writerPlayer2 = new PrintWriter(players.get(1).getOutputStream(),true);
+
+      writerPlayer1.println("Lost in Transition: Game is about to start");
+      writerPlayer2.println("Lost in Transition: Game is about to start");
+
       Random random = new Random();
-      String originalWord = wordList[random.nextInt(wordList.length)];
+      String word1 = wordList[random.nextInt(wordList.length)];
+      String word2 = wordList[random.nextInt(wordList.length)];
 
-      String current = originalWord;
-      for (int i = 0; i < players.size() - 1; i++);{
-        int limit = wordLimit[i];
-        
+      //Round 1
+
+      writerPlayer1.println("Round 1 - Describe this word in 10 words or less withouth saying the word: " + word1);
+      writerPlayer2.println("Round 1 - Describe this word in 10 words or less withouth saying the word: " + word2);
+
+      String description1 = readerPlayer1.readLine();
+      String description2 = readerPlayer2.readLine();
+
+      //Round 2
+
+      writerPlayer1.println("Round 2 - What is this? " + description2);
+      writerPlayer2.println("Round 2 - What is this? " + description1);
+
+      String guess1 = readerPlayer1.readLine();
+      String guess2 = readerPlayer2.readLine();
+
+      //Final Resutls
+
+      writerPlayer1.println("Your original word was " + word1 + ". It was lost to " + guess2);
+
+      writerPlayer2.println("Your original word was " + word2 + ". It was lost to " + guess1);
+
+      for(int i = 0; i < amountOfPlayers; i++){
+        players.get(i).close();
       }
 
-    } catch(Exception e){
+    }catch(Exception e){
       System.out.println("Game error");
     }
   }
