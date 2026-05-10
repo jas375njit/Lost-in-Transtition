8818f9f191e0d934e78a8e7ad70637801410b8c3
Joel <jas375@njit.edu>
2026-05-09 23:16:33 -0400

    Gamer server now allows multiple players (as many as the first player wants)

--- GameServer.java
+++ GameServer.java
@@ -1,31 +1,37 @@
+import java.io.*;
 import java.net.*;
 
 public class GameServer {
     private static int port = 1728;
     private static ServerSocket listener;
-    private static Socket connection;
 
     public static void main(String[] args) {
-        try{
+        try {
             listener = new ServerSocket(port);
-            System.out.println("Los in Transition server started on port " + listener.getLocalPort());
+            System.out.println("Lost in Transition server started on port " + listener.getLocalPort());
 
-            while (true){
+            while (true) {
                 GameRoom room = new GameRoom();
                 System.out.println("Waiting for players...");
 
-            //This while loop connects both players (more players overtime) at the same time
-            while (room.getPlayerCount() < 2){
-                connection = listener.accept();
-                room.addPlayer(connection);
+                Socket firstSocket = listener.accept();
+                BufferedReader firstIn = new BufferedReader(new InputStreamReader(firstSocket.getInputStream()));
+                int totalPlayers = Integer.parseInt(firstIn.readLine());
+                room.addPlayer(firstSocket, firstIn);
                 System.out.println("Players Connected: " + room.getPlayerCount());
-            }
+
+                //This while loop connects both players (more players overtime) at the same time
+                while (room.getPlayerCount() < totalPlayers) {
+                    Socket s = listener.accept();
+                    BufferedReader r = new BufferedReader(new InputStreamReader(s.getInputStream()));
+                    room.addPlayer(s, r);
+                    System.out.println("Players Connected: " + room.getPlayerCount());
+                }
+
                 System.out.println("Starting game with " + room.getPlayerCount() + " players");
-
                 new Thread(() -> room.game()).start();
-
             }
- 
+
         } catch (Exception e) {
             System.out.println("ERROR: Connection could not be established.");
             return;
